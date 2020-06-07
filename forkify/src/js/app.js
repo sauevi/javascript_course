c4111d7b2fcba4fd83508b1e73b43d2cbdba3e5f
Saul Eduardo Villamizar Camelo <saul.villamizar@globant.com>
2020-06-06 21:14:13 -0500

    ✨ adding recipe view

--- forkify/src/js/app.js
+++ forkify/src/js/app.js
@@ -1,5 +1,6 @@
-import { getRecipesByQuery } from '../recipe/routes/recipeRoute';
+import { getRecipesByQuery, getRecipeById } from '../recipe/routes/recipeRoute';
 import * as searchView from '../views/searchView';
+import * as recipeView from '../views/recipeView';
 import {
   elements,
   elementStrings,
@@ -22,15 +23,11 @@ const controlSearch = async () => {
   }
   const response = await getRecipesByQuery(query);
 
-  if (response.error) {
-    searchView.clearInput();
-    searchView.clearResults();
-    alert(response.message);
+  if (errorResponse(response)) {
     return;
   }
 
   state.recepies = response;
-
   searchView.clearInput();
   searchView.clearResults();
   renderLoader(elements.searchResult);
@@ -38,6 +35,26 @@ const controlSearch = async () => {
   searchView.renderResults(state.recepies);
 };
 
+const controlRecipe = async () => {
+  const id = window.location.hash.replace('#', '');
+
+  if (!id) {
+    return;
+  }
+
+  const response = await getRecipeById(id);
+
+  if (errorResponse(response)) {
+    return;
+  }
+
+  state.recipe = response;
+  state.recipe.calculateTime();
+  state.recipe.calculateServings();
+  // render
+};
+
+// events
 const { searchForm, searchResPages } = elements;
 const { btnPages } = elementStrings;
 
@@ -55,3 +72,18 @@ searchResPages.addEventListener('click', event => {
     searchView.renderResults(recepies, goToPage);
   }
 });
+
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, controlRecipe)
+);
+
+function errorResponse(response) {
+  if (response.error) {
+    searchView.clearInput();
+    searchView.clearResults();
+    alert(response.message);
+    return true;
+  }
+
+  return false;
+}
